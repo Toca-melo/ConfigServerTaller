272eafff6a52d2503919ce717c1f51cd505ed801
Usuario <brayanzamora0612@gmail.com>
2024-05-24 21:37:49 -0500

    cambio archivos

--- MicroservicioSpringRespuesta/src/main/java/com/zamora/respuesta/repository/ExamenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.zamora.respuesta.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.zamora.common.usuario.models.entity.Examen;
-
-public interface ExamenRepository extends CrudRepository<Examen, Long> {
-
-}
--- MicroservicioSpringRespuesta/src/main/java/com/zamora/respuesta/repository/PreguntaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.zamora.respuesta.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.zamora.common.usuario.models.entity.Pregunta; 
-
-public interface PreguntaRepository extends CrudRepository<Pregunta, Long>{
-
-}
